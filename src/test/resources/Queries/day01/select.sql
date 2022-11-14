56b144035004d2cf481cb5a860f4eda0c36cdcf6
Luzvin Urban <lucybdk@gmail.com>
2022-11-14 15:41:41 -0500

    MY SQL

---
+++ src/test/resources/Queries/day01/select.sql
@@ -0,0 +1,25 @@
+select * from employees;
+
+
+select * FROM employees;
+--Single line comments
+--SQL language is NOT CASE SENSITIVE
+select  * FROM eMpLoYees;
+
+--display all information from departments table
+select  * from departments;
+
+--display firstname from employees table
+select first_name from employees;
+
+select FIRST_NAME from EMPLOYEES;
+
+--display firstname,lastname and salary from employees table
+select first_name, last_name,salary from employees;
+
+--display all information from locations table.
+select * from locations;
+
+--display country name and region id from countries
+select  job_id, job_title from jobs;
+
